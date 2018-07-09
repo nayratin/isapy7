48fc79884bd98237f5144142e9b33e2a48365388
nayratin <40776299+nayratin@users.noreply.github.com>
2018-07-09 16:02:19 +0200

    final ver

--- zad3.py
+++ zad3.py
@@ -1,5 +1,5 @@
-promien = int((input("Podaj promień koła: "))
-pole = 3.14 * promien ** 2
+promien = int(input("Podaj długość promienia koła: "))
+pole = str(3.14 * promien ** 2)
 
 print("Przymujemy wartość Pi jako 3.14")
 print("Pole wynosi " + pole)
