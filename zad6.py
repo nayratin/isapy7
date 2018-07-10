12e6a9790368f2e372a2a523fb38697602fc869a
nayratin <40776299+nayratin@users.noreply.github.com>
2018-07-10 13:13:26 +0200

    literowki

--- zad6.py
+++ zad6.py
@@ -1,7 +1,7 @@
-binarna = input("Podaj liczbê zapisan¹ w systemie binarnym o d³ugoœci 6 znaków: ")
+binarna = input("Podaj liczbÄ™ zapisanÄ… w systemie binarnym o dÅ‚ugoÅ›ci 6 znakÃ³w: ")
 
 if len(binarna) == 6:
   dziesietna = 2 ** 5 * int(binarna[0]) + 2 ** 4 * int(binarna[1]) + 2 ** 3 * int(binarna[2]) + 2 ** 2 * int(binarna[3]) + + 2 ** 1 * int(binarna[4]) + 2 ** 0 * int(binarna[5])
   print(str(binarna) + " to " + str(dziesietna) + ".")
 else:
-  print("Twoja liczba jest za niepoprawnej d³ugoœci!")
+  print("Twoja liczba jest niepoprawnej dÅ‚ugoÅ›ci!")
