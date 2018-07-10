9ff226b6f09817a0dc717f0314ad81c87b6402a5
Paola <nayratin@gmail.com>
2018-07-10 17:48:31 +0100

    syntax

--- zad2.py
+++ zad2.py
@@ -1,4 +1,4 @@
-fahrenheit = int(input("Podaj temperaturê w stopniach Fahrenheita: "))
+fahrenheit = int(input("Podaj temperaturÄ™ w stopniach Fahrenheita: "))
 celsjusz = (fahrenheit - 32) / 9 / 5
 
-print(str(fahrenheit) + " stopni Fahrenheita to oko³o " + str(celsjusz) + "  stopni Celsjusza.")
+print(str(fahrenheit) + " stopni Fahrenheita to okoÅ‚o " + str(celsjusz) + "  stopni Celsjusza.")
