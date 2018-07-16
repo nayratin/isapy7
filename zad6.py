8c4d6e63815faaec6369f130956535c8841dad56
nayratin <40776299+nayratin@users.noreply.github.com>
2018-07-16 20:13:32 +0200

    Delete zad6.py

--- zad6.py
+++
@@ -1,7 +0,0 @@
-binarna = input("Podaj liczbę zapisaną w systemie binarnym o długości 6 znaków: ")
-
-if len(binarna) == 6:
-  dziesietna = 2 ** 5 * int(binarna[0]) + 2 ** 4 * int(binarna[1]) + 2 ** 3 * int(binarna[2]) + 2 ** 2 * int(binarna[3]) + + 2 ** 1 * int(binarna[4]) + 2 ** 0 * int(binarna[5])
-  print(str(binarna) + " to " + str(dziesietna) + ".")
-else:
-  print("Twoja liczba jest niepoprawnej długości!")
