062ec427e6704665cbf0250ef6ffeb0c5c30d374
nayratin <40776299+nayratin@users.noreply.github.com>
2018-07-16 20:13:21 +0200

    Delete zad5.py

--- zad5.py
+++
@@ -1,8 +0,0 @@
-szerokosc = int(input("Podaj szerokość prostokąta: "))
-dlugosc = int(input("Podaj długość prostokąta: "))
-
-podstawa_gorna = "+" + "-" * szerokosc + "+" + "\n"
-podstawa_dolna = "+" + "-" * szerokosc + "+"
-boki = "|" + " " * szerokosc + "|" + "\n"
-
-print(podstawa_gorna + boki * dlugosc + podstawa_dolna)
