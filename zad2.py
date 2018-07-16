e64bdcc00d65ea0c227be3f7dc6d5de2e0ceffbb
nayratin <40776299+nayratin@users.noreply.github.com>
2018-07-16 20:12:33 +0200

    Delete zad2.py

--- zad2.py
+++
@@ -1,4 +0,0 @@
-fahrenheit = int(input("Podaj temperaturę w stopniach Fahrenheita: "))
-celsjusz = (fahrenheit - 32) / 9 / 5
-
-print(str(fahrenheit) + " stopni Fahrenheita to około " + str(celsjusz) + "  stopni Celsjusza.")
