5fbf7d1b839f3429b30c98072754406fb93d8b93
Paola <nayratin@gmail.com>
2018-07-16 20:52:20 +0200

    wip

--- zadD4/wiekPsa.py
+++ zadD4/wiekPsa.py
@@ -1,13 +1,15 @@
 def dog_age_calc(y):
   """przeliczamy wiek psa zakladajac ze przez pierwsze dwa lata każdy rok to 10.5 lat, a kazdy nastepny to 4"""
-  if y <= 2:
-    age = y * 10.5
-  elif y > 2:
+
+  if y > 0 and y <= 2:
     age = 2 * 10.5 + (y - 2) * 4
   else:
-    pass
-  print(str(yoursDogAge) + " psich lat to " + str(age) + " ludzkich.")
+    return "niepoprawne dane!"
+  print(str(y) + " psich lat to " + str(age) + " ludzkich.")
 
-yoursDogAge = int(input("Podaj wiek psa: "))
+yoursDogAge = input("Podaj wiek psa: ")
 
-dog_age_calc(yoursDogAge)
+if yoursDogAge.isdigit() == True:
+  dog_age_calc(yoursDogAge)
+else:
+  print("bledne dane!")
